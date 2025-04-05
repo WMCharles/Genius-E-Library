fb4bd15f0904c93ec73e3eec3fad5ad39cc45b18
majimbojr <charleswafula73@gmail.com>
2025-04-05 11:21:33 +0300

    done

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,6 +3,7 @@ import { Card } from 'react-bootstrap'
 export default function Box({ title, text }) {
     return (
         <Card className="mb-4 shadow-sm advert-box">
+            xxxxxxxxxxxx
             <Card.Body>
                 <Card.Title className="text-primary">{title}</Card.Title>
                 <Card.Text className="text-muted">
